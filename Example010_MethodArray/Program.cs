304474ba01311f781363e7c61d74aa5bd1823003
Grigoriev_Sergey <gsn1232@gmail.com>
2023-03-11 19:14:05 +0200

    ArrayLibrary

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 92, 83, 45, 5, 56, 97, 83, 9 };
+int n = array.Length;
+int find = 83;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
